6f6b9c6b933a4e09dece38b23c92418cb7be3120
MillerMark <coderush@gmail.com>
2025-10-29 07:03:55 +0100

    Added getting started

---
+++ DXWpf/Start.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace DXWpf {
+    internal class Start {
+        /* 
+         Getting Started with CodeRush
+            [ ] Run the Setup Wizard
+
+            [ ] Getting Started With CodeRush Video
+
+                ![](C02C3439468F74B1B658396A3F8D6FE1.png;https://www.youtube.com/watch?v=3o26HaNdxqk )
+                https://www.youtube.com/watch?v=3o26HaNdxqk
+
+            [ ] CodeRush Feature of the Week Video Series
+
+               ![](B2578AC4B2A52754A4F065075077F395.png;https://www.youtube.com/playlist?list=PL8h4jt35t1wgawacCN9wmxq1EN36CNUGk )
+                https://www.youtube.com/playlist?list=PL8h4jt35t1wgawacCN9wmxq1EN36CNUGk
+
+            [ ] Open the CodeRush Learning Solution
+
+            [ ] Get the Cheat Sheet:
+                ![](188183DAA6A097C1A2AF614C530FF434.png;https://community.devexpress.com/blogs/markmiller/archive/2021/12/10/coderush-cheat-sheet-v4-0.aspx )
+                https://community.devexpress.com/blogs/markmiller/archive/2021/12/10/coderush-cheat-sheet-v4-0.aspx
+         
+         Be sure to mention these CodeRush features:
+         
+            [ ] Tab to Next Reference
+            [ ] Caps as a Modifier
+            [ ] Test Runner
+            [ ] Voice Commands
+
+          
+         
+         
+         */
+    }
+}
